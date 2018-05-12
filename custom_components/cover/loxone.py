101aec9aecf95044d3d76cb6b0657d0fd2f0ca9a
Joachim <32484282+JoDehli@users.noreply.github.com>
2018-05-12 09:46:12 +0200

    gate added

--- custom_components/cover/loxone.py
+++ custom_components/cover/loxone.py
@@ -2,7 +2,6 @@
 Loxone cover component.
 """
 import asyncio
-import json
 import logging
 
 from homeassistant.components.cover import (
@@ -52,36 +51,157 @@ def async_setup_platform(hass, config, async_add_devices, discovery_info=None):
 
     for cover in get_all_covers(loxconfig):
         if cover['type'] == "Gate":
-            pass
-            # new_cover = LoxoneCover(hass, cover['name'],
-            #                         cover['uuidAction'],
-            #                         position_uuid=cover['states']['position'],
-            #                         device_class="Gate")
+            new_gate = LoxoneGate(hass, cover['name'],
+                                  cover['uuidAction'],
+                                  position_uuid=cover['states']['position'],
+                                  state_uuid=cover['states']['active'],
+                                  device_class="Gate",
+                                  complete_data=cover)
+            devices.append(new_gate)
+            hass.bus.async_listen(EVENT, new_gate.event_handler)
         else:
-            new_cover = LoxoneCover(hass, cover['name'],
-                                    cover['uuidAction'],
-                                    position_uuid=cover['states'][
-                                        'position'],
-                                    shade_uuid=cover['states'][
-                                        'shadePosition'],
-                                    down_uuid=cover['states']['down'],
-                                    up_uuid=cover['states']['up'],
-                                    device_class="Jalousie")
+            new_jalousie = LoxoneJalousie(hass, cover['name'],
+                                          cover['uuidAction'],
+                                          position_uuid=cover['states'][
+                                              'position'],
+                                          shade_uuid=cover['states'][
+                                              'shadePosition'],
+                                          down_uuid=cover['states']['down'],
+                                          up_uuid=cover['states']['up'],
+                                          device_class="Jalousie",
+                                          complete_data=cover)
 
-            devices.append(new_cover)
-            hass.bus.async_listen(EVENT, new_cover.event_handler)
+            devices.append(new_jalousie)
+            hass.bus.async_listen(EVENT, new_jalousie.event_handler)
 
     async_add_devices(devices)
 
 
-class LoxoneCover(CoverDevice):
-    """Loxone Cover"""
+class LoxoneGate(CoverDevice):
+    """Loxone Jalousie"""
+
+    def __init__(self, hass, name, uuid, position_uuid=None, state_uuid=None,
+                 device_class=None, complete_data=None):
+        self.hass = hass
+        self._name = name
+        self._uuid = uuid
+        self._position_uuid = position_uuid
+        self._state_uuid = state_uuid
+        self._device_class = device_class
+        self._complete_data = complete_data
+        self._position = None
+        self._is_opening = False
+        self._is_closing = False
+
+        if self._position is None:
+            self._closed = True
+        else:
+            self._closed = self.current_cover_position <= 0
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return SUPPORT_OPEN | SUPPORT_CLOSE | SUPPORT_STOP
+
+    @property
+    def name(self):
+        """Return the name of the cover."""
+        return self._name
+
+    @property
+    def should_poll(self):
+        """No polling needed for a demo cover."""
+        return False
+
+    @property
+    def device_class(self):
+        """Return the class of this device, from component DEVICE_CLASSES."""
+        return self._device_class
+
+    @property
+    def current_cover_position(self):
+        """Return the current position of the cover."""
+        return self._position
+
+    @property
+    def is_closed(self):
+        """Return if the cover is closed."""
+        return self._closed
+
+    @property
+    def is_closing(self):
+        """Return if the cover is closing."""
+        return self._is_closing
+
+    @property
+    def is_opening(self):
+        """Return if the cover is opening."""
+        return self._is_opening
+
+    def open_cover(self, **kwargs):
+        """Open the cover."""
+        if self._position == 100.:
+            return
+        elif self._position is None:
+            self._closed = False
+            self.schedule_update_ha_state()
+            return
+        self.hass.bus.async_fire(SENDDOMAIN,
+                                 dict(uuid=self._uuid, value="open"))
+        self.schedule_update_ha_state()
+
+    def close_cover(self, **kwargs):
+        """Close the cover."""
+        if self._position == 0:
+            return
+        elif self._position is None:
+            self._closed = True
+            self.schedule_update_ha_state()
+            return
+        self.hass.bus.async_fire(SENDDOMAIN,
+                                 dict(uuid=self._uuid, value="down"))
+        self.schedule_update_ha_state()
+
+    def stop_cover(self, **kwargs):
+        """Stop the cover."""
+        if self.is_closing:
+            self.hass.bus.async_fire(SENDDOMAIN,
+                                     dict(uuid=self._uuid, value="up"))
+            return
+
+        if self.is_opening:
+            self.hass.bus.async_fire(SENDDOMAIN,
+                                     dict(uuid=self._uuid, value="down"))
+            return
+
+    @asyncio.coroutine
+    def event_handler(self, event):
+        if self._position_uuid in event.data or self._state_uuid in event.data:
+            if self._position_uuid in event.data:
+                self._position = float(event.data[self._position_uuid]) * 100.
+                if self._position == 0:
+                    self._closed = True
+                else:
+                    self._closed = False
+
+            if self._state_uuid in event.data:
+                self._is_closing = False
+                self._is_opening = False
+
+                if event.data[self._state_uuid] == -1:
+                    self.is_opening = True
+                elif event.data[self._state_uuid] == 1:
+                    self._is_opening = True
+            self.schedule_update_ha_state()
+
+
+class LoxoneJalousie(CoverDevice):
+    """Loxone Jalousie"""
 
     # pylint: disable=no-self-use
     def __init__(self, hass, name, uuid, position_uuid=None,
                  shade_uuid=None, down_uuid=None, up_uuid=None,
-                 position=None, tilt_position=None, device_class=None,
-                 supported_features=None):
+                 device_class=None,complete_data=None):
         self.hass = hass
         self._name = name
         self._uuid = uuid
@@ -89,19 +209,19 @@ class LoxoneCover(CoverDevice):
         self._shade_uuid = shade_uuid
         self._down_uuid = down_uuid
         self._up_uuid = up_uuid
-        self._position = position
         self._device_class = device_class
-        self._supported_features = supported_features
+        self._position = None
         self._set_position = None
         self._set_tilt_position = None
-        self._tilt_position = tilt_position
+        self._tilt_position = None
         self._requested_closing = True
         self._requested_closing_tilt = True
         self._unsub_listener_cover = None
         self._unsub_listener_cover_tilt = None
         self._is_opening = False
         self._is_closing = False
-        if position is None:
+        self._complete_data = complete_data
+        if self._position is None:
             self._closed = True
         else:
             self._closed = self.current_cover_position <= 0
