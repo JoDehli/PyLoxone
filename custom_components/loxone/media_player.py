95ca1ae16587001cabcbcd087a9f0c57a524ca2f
JoDehli <joachim.dehli@gmail.com>
2023-11-05 11:45:31 +0100

    Little changes

--- custom_components/loxone/media_player.py
+++ custom_components/loxone/media_player.py
@@ -1,28 +1,22 @@
 """Support for Loxone Audio zone media player."""
 from __future__ import annotations
 
-from collections.abc import Mapping
 import logging
-from typing import TYPE_CHECKING, Any
 
-from homeassistant.components.media_player import (
-    MediaPlayerDeviceClass,
-    MediaPlayerEntity,
-    MediaPlayerEntityFeature,
-    MediaPlayerState,
-)
+from homeassistant.components.media_player import (MediaPlayerDeviceClass,
+                                                   MediaPlayerEntity,
+                                                   MediaPlayerEntityFeature,
+                                                   MediaPlayerState)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from . import LoxoneEntity
-from .const import DOMAIN, SENDDOMAIN, DEFAULT_AUDIO_ZONE_V2_PLAY_STATE
-from .helpers import (
-    get_all,
-    get_cat_name_from_cat_uuid,
-    get_room_name_from_room_uuid,
-)
+from .const import DEFAULT_AUDIO_ZONE_V2_PLAY_STATE, DOMAIN, SENDDOMAIN
+from .helpers import (get_all, get_cat_name_from_cat_uuid,
+                      get_room_name_from_room_uuid)
 from .miniserver import get_miniserver_from_hass
 
 _LOGGER = logging.getLogger(__name__)
@@ -42,6 +36,7 @@ SUPPORT_LOXONE_AUDIO_ZONE = (
     | MediaPlayerEntityFeature.VOLUME_STEP
 )
 
+
 async def async_setup_platform(
     hass: HomeAssistant,
     config: ConfigType,
@@ -50,6 +45,7 @@ async def async_setup_platform(
 ) -> None:
     """Set up Loxone Audio zones."""
     return True
+
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -66,13 +62,30 @@ async def async_setup_entry(
             {
                 "hass": hass,
                 "typ": "AudioZoneV2",
-                "room": get_room_name_from_room_uuid(loxconfig, audioZone.get("room", "")),
-                "cat": get_cat_name_from_cat_uuid(loxconfig, audioZone.get("cat", ""))
+                "room": get_room_name_from_room_uuid(
+                    loxconfig, audioZone.get("room", "")
+                ),
+                "cat": get_cat_name_from_cat_uuid(loxconfig, audioZone.get("cat", "")),
             }
         )
         entites.append(LoxoneAudioZoneV2(**audioZone))
 
     async_add_entities(entites)
+
+
+def play_state_to_media_player_state(play_state: int) -> MediaPlayerState:
+    match play_state:
+        case 0:
+            return MediaPlayerState.IDLE
+        case 1:
+            return MediaPlayerState.PAUSED
+        case 2:
+            return MediaPlayerState.PLAYING
+        case -1:
+            return MediaPlayerState.OFF
+        case _:
+            _LOGGER.warning(f"Unknown playState:{play_state}")
+
 
 class LoxoneAudioZoneV2(LoxoneEntity, MediaPlayerEntity):
     """Representation of a AudioZoneV2 Loxone device."""
@@ -83,7 +96,7 @@ class LoxoneAudioZoneV2(LoxoneEntity, MediaPlayerEntity):
         self.hass = kwargs["hass"]
 
         self._attr_device_class = MediaPlayerDeviceClass.SPEAKER
-        self._state = self.playStateToMediaPlayerState(DEFAULT_AUDIO_ZONE_V2_PLAY_STATE)
+        self._state = play_state_to_media_player_state(DEFAULT_AUDIO_ZONE_V2_PLAY_STATE)
         self._volume = 0
 
         self._attr_device_info = DeviceInfo(
@@ -91,42 +104,31 @@ class LoxoneAudioZoneV2(LoxoneEntity, MediaPlayerEntity):
             name=f"{DOMAIN} {self.name}",
             manufacturer="Loxone",
             suggested_area=self.room,
-            model=self.typ
+            model=self.typ,
         )
 
     async def event_handler(self, event):
         should_update = False
 
         if self.states["volume"] in event.data:
-            self._volume = float(event.data[self.states['volume']]) / 100
+            self._volume = float(event.data[self.states["volume"]]) / 100
             should_update = True
 
         if self.states["playState"] in event.data:
-            self._state = self.playStateToMediaPlayerState(event.data[self.states['playState']])
+            self._state = play_state_to_media_player_state(
+                event.data[self.states["playState"]]
+            )
             should_update = True
 
         if should_update:
             self.async_schedule_update_ha_state()
 
-    def playStateToMediaPlayerState(self, playState) -> MediaPlayerState:
-        match playState:
-            case 0:
-                return MediaPlayerState.IDLE
-            case 1:
-                return MediaPlayerState.PAUSED
-            case 2:
-                return MediaPlayerState.PLAYING
-            case -1:
-                return MediaPlayerState.OFF
-            case _:
-                _LOGGER.warning(f"Unknown playState:{playState}")
-    
-    #properties
+    # properties
     @property
     def state(self) -> MediaPlayerState:
         """Return the playback state."""
         return self._state
-    
+
     @property
     def volume_level(self) -> float | None:
         """Volume level of the media player (0..1)."""
@@ -137,7 +139,7 @@ class LoxoneAudioZoneV2(LoxoneEntity, MediaPlayerEntity):
         """Flag media player features that are supported."""
         return SUPPORT_LOXONE_AUDIO_ZONE
 
-    #commands
+    # commands
     async def async_media_play(self) -> None:
         """Send play command to device."""
         self.hass.bus.async_fire(SENDDOMAIN, dict(uuid=self.uuidAction, value="play"))
@@ -162,11 +164,13 @@ class LoxoneAudioZoneV2(LoxoneEntity, MediaPlayerEntity):
         """Send previous track command to device."""
         self.hass.bus.async_fire(SENDDOMAIN, dict(uuid=self.uuidAction, value="prev"))
         self.async_schedule_update_ha_state()
-    
+
     async def async_set_volume_level(self, volume: float) -> None:
         """Send new volume_level to device."""
-        volumeInt = int(volume * 100)
-        self.hass.bus.async_fire(SENDDOMAIN, dict(uuid=self.uuidAction, value=f"volume/{volumeInt}"))
+        volume_int = int(volume * 100)
+        self.hass.bus.async_fire(
+            SENDDOMAIN, dict(uuid=self.uuidAction, value=f"volume/{volume_int}")
+        )
         self.async_schedule_update_ha_state()
 
     async def async_volume_up(self) -> None:
@@ -176,5 +180,7 @@ class LoxoneAudioZoneV2(LoxoneEntity, MediaPlayerEntity):
 
     async def async_volume_down(self) -> None:
         """Send volume DOWN to device."""
-        self.hass.bus.async_fire(SENDDOMAIN, dict(uuid=self.uuidAction, value="volDown"))
+        self.hass.bus.async_fire(
+            SENDDOMAIN, dict(uuid=self.uuidAction, value="volDown")
+        )
         self.async_schedule_update_ha_state()
