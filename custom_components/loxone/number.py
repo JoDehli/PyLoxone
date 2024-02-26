b6cf7d6d894693e1db3b5119ef1bc4c31ec265d8
gigatexel <65073191+gigatexel@users.noreply.github.com>
2024-02-26 10:25:11 +0100

    Create number.py

---
+++ custom_components/loxone/number.py
@@ -0,0 +1,151 @@
+"""
+Loxone Numbers
+
+For more details about this component, please refer to the documentation at
+https://github.com/JoDehli/PyLoxone
+"""
+import logging
+
+from homeassistant.components.number import NumberEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import STATE_UNKNOWN
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+from . import LoxoneEntity
+from .const import DOMAIN, SENDDOMAIN
+from .helpers import (get_all, get_cat_name_from_cat_uuid,get_room_name_from_room_uuid)
+from .miniserver import get_miniserver_from_hass
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up Loxone Number."""
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up entry."""
+    miniserver = get_miniserver_from_hass(hass)
+    loxconfig = miniserver.lox_config.json
+    entites = []
+
+    for number_entity in get_all(
+        loxconfig, ["Slider"]
+    ):    
+        number_entity.update(
+            {
+                "room": get_room_name_from_room_uuid(
+                    loxconfig, number_entity.get("room", "")
+                ),
+                "cat": get_cat_name_from_cat_uuid(
+                    loxconfig, number_entity.get("cat", "")
+                ),
+                "config_entry": config_entry,
+            }
+        )
+        new_number = LoxoneNumber(**number_entity)
+        entites.append(new_number)
+
+    async_add_entities(entites)
+
+class LoxoneNumber(LoxoneEntity, NumberEntity):
+    """Representation of a loxone number"""
+
+    def __init__(self, **kwargs):
+        LoxoneEntity.__init__(self, **kwargs)
+        """Initialize the Loxone number."""
+        self._state = STATE_UNKNOWN
+        self._icon = None
+        self._assumed = False
+        self._native_max_value = kwargs["details"]["max"]
+        self._native_min_value = kwargs["details"]["min"]
+
+    @property
+    def should_poll(self):
+        """No polling needed for a demo number."""
+        return False
+
+    @property
+    def icon(self):
+        """Return the icon to use for device if any."""
+        return self._icon
+
+    @property
+    def native_max_value(self):
+        """Return the native_max_value to use for device if any."""
+        return self._native_max_value
+
+    @property
+    def native_min_value(self):
+        """Return the native_min_value to use for device if any."""
+        return self._native_min_value
+
+
+    @property
+    def assumed_state(self):
+        """Return if the state is based on assumptions."""
+        return self._assumed
+
+    @property
+    def native_value(self):
+        """Return the state of the sensor."""
+        return self._state        
+
+    async def event_handler(self, e):
+        if self.uuidAction in e.data:
+            data = e.data[self.uuidAction]
+            if isinstance(data, (list, dict)):
+                data = str(data)
+                if len(data) >= 255:
+                    self._state = data[:255]
+                else:
+                    self._state = data
+            else:
+                self._state = data
+
+            self.schedule_update_ha_state()
+
+    @property
+    def extra_state_attributes(self):
+        """Return device specific state attributes.
+
+        Implemented by platform classes.
+        """
+        return {
+            "uuid": self.uuidAction,
+            "state_uuid": self.states["value"],
+            "room": self.room,
+            "category": self.cat,
+            "device_typ": self.type,
+            "platform": "loxone",
+        }
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "name": self.name,
+            "manufacturer": "Loxone",
+            "model": self.type,
+            "suggested_area": self.room,
+        }
+
+    async def async_set_native_value(self, value: float):
+        """Set new value."""
+        self.hass.bus.async_fire(
+            SENDDOMAIN, dict(uuid=self.uuidAction, value="{}".format(value))
+        )
+        self.async_schedule_update_ha_state()
