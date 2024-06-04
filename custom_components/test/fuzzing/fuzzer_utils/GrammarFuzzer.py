6f56c5e80181ddaefb94a27969365c2d8ed3720d
ThorbenCarl <thorben.carl@gmail.com>
2024-06-04 15:35:50 +0200

    marked _NON_TERMINAL_REGEX as private, run auto formatter, add dummy docstring

--- custom_components/test/fuzzing/fuzzer_utils/GrammarFuzzer.py
+++ custom_components/test/fuzzing/fuzzer_utils/GrammarFuzzer.py
@@ -16,18 +16,22 @@ class CostGrammarType(Enum):
     MAX = 2
 
 
-class GrammarFuzzer():
-    NON_TERMINAL_REGEX = re.compile(r"<.*?>")
+class GrammarFuzzer:
+    _NON_TERMINAL_REGEX = re.compile(r"<.*?>")
 
     def __init__(self) -> None:
+        """constructor"""
         pass
 
-    def __convert_to_cost_grammar(self, grammar: Grammar, conversion_type: CostGrammarType) -> Tuple[
-        Annotated_Grammar, Annotated_Non_Terminals]:
+    def __convert_to_cost_grammar(
+        self, grammar: Grammar, conversion_type: CostGrammarType
+    ) -> Tuple[Annotated_Grammar, Annotated_Non_Terminals]:
+        """TODO: Add summary"""
         cost_grammar: Annotated_Grammar = {}
         annotated_non_terminals: Annotated_Non_Terminals = {}
 
         def convert_rule(head: Element, body: List[Element]) -> None:
+            """TODO: Add summary"""
             if head in annotated_non_terminals:
                 return
 
@@ -37,7 +41,7 @@ class GrammarFuzzer():
 
             for element in body:
                 cost = 0
-                non_terminals = re.findall(self.NON_TERMINAL_REGEX, element)
+                non_terminals = re.findall(self._NON_TERMINAL_REGEX, element)
 
                 if head in non_terminals:
                     is_inf = True
@@ -53,27 +57,33 @@ class GrammarFuzzer():
             cost_grammar[head] = annotated_elements
 
             if not is_inf:
-                annotated_non_terminals[head] = min(annotated_elements, key=lambda x: x[1])[
-                                                    1] + 1 if conversion_type == CostGrammarType.MIN else \
-                    max(annotated_elements, key=lambda x: x[1])[1] + 1
+                annotated_non_terminals[head] = (
+                    min(annotated_elements, key=lambda x: x[1])[1] + 1
+                    if conversion_type == CostGrammarType.MIN
+                    else max(annotated_elements, key=lambda x: x[1])[1] + 1
+                )
 
         for key, value in grammar.items():
             convert_rule(key, value)
 
         return cost_grammar, annotated_non_terminals
 
-    def __convert_to_trackable_grammar(self, grammar: Grammar) -> Tuple[Annotated_Grammar, Annotated_Non_Terminals]:
+    def __convert_to_trackable_grammar(
+        self, grammar: Grammar
+    ) -> Tuple[Annotated_Grammar, Annotated_Non_Terminals]:
+        """TODO: Add summary"""
         trackable_grammar: Annotated_Grammar = {}
         trackable_non_terminals: Annotated_Non_Terminals = {}
 
         def convert_rule(head: Element, body: List[Element]) -> None:
+            """TODO: Add summary"""
             if head in trackable_non_terminals:
                 return
 
             trackable_elements: List[Annotated_Element] = []
 
             for element in body:
-                non_terminals = re.findall(self.NON_TERMINAL_REGEX, element)
+                non_terminals = re.findall(self._NON_TERMINAL_REGEX, element)
 
                 for non_terminal in non_terminals:
                     if non_terminal not in trackable_non_terminals:
@@ -88,99 +98,187 @@ class GrammarFuzzer():
 
         return trackable_grammar, trackable_non_terminals
 
-    def __compose_min_cost(self, head: Element, given_cost_grammar: Annotated_Grammar) -> str:
+    def __compose_min_cost(
+        self, head: Element, given_cost_grammar: Annotated_Grammar
+    ) -> str:
+        """TODO: Add summary"""
         min_tuple: Annotated_Element = min(given_cost_grammar[head], key=lambda x: x[1])
-        is_non_terminal = True if re.findall(self.NON_TERMINAL_REGEX, min_tuple[0]) else False
+        is_non_terminal = (
+            True if re.findall(self._NON_TERMINAL_REGEX, min_tuple[0]) else False
+        )
 
         if not is_non_terminal:
             return min_tuple[0]
         else:
-            non_terminals = re.findall(self.NON_TERMINAL_REGEX, min_tuple[0])
+            non_terminals = re.findall(self._NON_TERMINAL_REGEX, min_tuple[0])
             replacements = iter(
-                list(map(lambda element: self.__compose_min_cost(element, given_cost_grammar), non_terminals)))
-            result = re.sub(self.NON_TERMINAL_REGEX, lambda element: next(replacements), min_tuple[0])
+                list(
+                    map(
+                        lambda element: self.__compose_min_cost(
+                            element, given_cost_grammar
+                        ),
+                        non_terminals,
+                    )
+                )
+            )
+            result = re.sub(
+                self._NON_TERMINAL_REGEX,
+                lambda element: next(replacements),
+                min_tuple[0],
+            )
             return result
 
     def fuzz_min_cost(self, grammar: Grammar, start_symbol: Element) -> str:
         """Derives the first mininum cost value of a given grammar."""
         cost_grammar: Annotated_Grammar
-        cost_grammar, _ = self.__convert_to_cost_grammar(
-            grammar, CostGrammarType.MIN)
+        cost_grammar, _ = self.__convert_to_cost_grammar(grammar, CostGrammarType.MIN)
 
         return self.__compose_min_cost(start_symbol, cost_grammar)
 
-    def __compose_max_cost(self, head: Element, given_cost_grammar: Annotated_Grammar, applications: int,
-                           max_applications: int) -> str:
+    def __compose_max_cost(
+        self,
+        head: Element,
+        given_cost_grammar: Annotated_Grammar,
+        applications: int,
+        max_applications: int,
+    ) -> str:
+        """TODO: Add summary"""
 
         if applications == max_applications:
-            min_tuple: Annotated_Element = min(given_cost_grammar[head], key=lambda x: x[1])
-            is_non_terminal = True if re.findall(self.NON_TERMINAL_REGEX, min_tuple[0]) else False
+            min_tuple: Annotated_Element = min(
+                given_cost_grammar[head], key=lambda x: x[1]
+            )
+            is_non_terminal = (
+                True if re.findall(self._NON_TERMINAL_REGEX, min_tuple[0]) else False
+            )
 
             if not is_non_terminal:
                 return min_tuple[0]
             else:
-                non_terminals = re.findall(self.NON_TERMINAL_REGEX, min_tuple[0])
+                non_terminals = re.findall(self._NON_TERMINAL_REGEX, min_tuple[0])
                 replacements = iter(
-                    list(map(lambda element: self.__compose_max_cost(element, given_cost_grammar, applications,
-                                                                     max_applications), non_terminals)))
-                result = re.sub(self.NON_TERMINAL_REGEX, lambda element: next(replacements), min_tuple[0])
+                    list(
+                        map(
+                            lambda element: self.__compose_max_cost(
+                                element,
+                                given_cost_grammar,
+                                applications,
+                                max_applications,
+                            ),
+                            non_terminals,
+                        )
+                    )
+                )
+                result = re.sub(
+                    self._NON_TERMINAL_REGEX,
+                    lambda element: next(replacements),
+                    min_tuple[0],
+                )
                 return result
         else:
-            max_tuple: Annotated_Element = max(given_cost_grammar[head], key=lambda x: x[1])
-            is_non_terminal = True if re.findall(self.NON_TERMINAL_REGEX, max_tuple[0]) else False
+            max_tuple: Annotated_Element = max(
+                given_cost_grammar[head], key=lambda x: x[1]
+            )
+            is_non_terminal = (
+                True if re.findall(self._NON_TERMINAL_REGEX, max_tuple[0]) else False
+            )
 
             if not is_non_terminal:
                 return max_tuple[0]
             else:
-                non_terminals = re.findall(self.NON_TERMINAL_REGEX, max_tuple[0])
+                non_terminals = re.findall(self._NON_TERMINAL_REGEX, max_tuple[0])
                 replacements = iter(
-                    list(map(lambda element: self.__compose_max_cost(element, given_cost_grammar, applications + 1,
-                                                                     max_applications), non_terminals)))
-                result = re.sub(self.NON_TERMINAL_REGEX, lambda element: next(replacements), max_tuple[0])
+                    list(
+                        map(
+                            lambda element: self.__compose_max_cost(
+                                element,
+                                given_cost_grammar,
+                                applications + 1,
+                                max_applications,
+                            ),
+                            non_terminals,
+                        )
+                    )
+                )
+                result = re.sub(
+                    self._NON_TERMINAL_REGEX,
+                    lambda element: next(replacements),
+                    max_tuple[0],
+                )
                 return result
 
-    def fuzz_max_cost(self, grammar: Grammar, start_symbol: Element, max_rule_applications: int) -> str:
+    def fuzz_max_cost(
+        self, grammar: Grammar, start_symbol: Element, max_rule_applications: int
+    ) -> str:
         """Derives the first maximum cost value of a given grammar."""
         cost_grammar: Annotated_Grammar
-        cost_grammar, _ = self.__convert_to_cost_grammar(
-            grammar, CostGrammarType.MAX)
+        cost_grammar, _ = self.__convert_to_cost_grammar(grammar, CostGrammarType.MAX)
 
-        return self.__compose_max_cost(start_symbol, cost_grammar, 0, max_rule_applications)
+        return self.__compose_max_cost(
+            start_symbol, cost_grammar, 0, max_rule_applications
+        )
 
-    def __is_grammar_covered(self, trackable_grammar: Annotated_Grammar,
-                             trackable_non_terminals: Annotated_Non_Terminals) -> bool:
+    def __is_grammar_covered(
+        self,
+        trackable_grammar: Annotated_Grammar,
+        trackable_non_terminals: Annotated_Non_Terminals,
+    ) -> bool:
+        """TODO: Add summary"""
         for non_terminal in trackable_non_terminals:
-            if non_terminal[1] == 0: return False
+            if non_terminal[1] == 0:
+                return False
 
         for rule in trackable_grammar:
             for element in trackable_grammar[rule]:
-                if element[1] == 0: return False
+                if element[1] == 0:
+                    return False
 
         return True
 
-    def fuzz_grammar_coverage(self, grammar: Grammar, start_symbol: Element) -> List[str]:
+    def fuzz_grammar_coverage(
+        self, grammar: Grammar, start_symbol: Element
+    ) -> List[str]:
         """Derives values until each production rule is fully covered."""
         trackable_grammar: Annotated_Grammar
         trackable_non_terminals: Annotated_Non_Terminals
-        trackable_grammar, trackable_non_terminals = self.__convert_to_trackable_grammar(grammar)
+        trackable_grammar, trackable_non_terminals = (
+            self.__convert_to_trackable_grammar(grammar)
+        )
 
         fuzzed_values: List[str] = []
 
         def generate_value(head: Element):
-            min_tuple: Annotated_Element = min(trackable_grammar[head], key=lambda x: x[1])
-            is_non_terminal = True if re.findall(self.NON_TERMINAL_REGEX, min_tuple[0]) else False
+            """TODO: Add summary"""
+            min_tuple: Annotated_Element = min(
+                trackable_grammar[head], key=lambda x: x[1]
+            )
+            is_non_terminal = (
+                True if re.findall(self._NON_TERMINAL_REGEX, min_tuple[0]) else False
+            )
             trackable_grammar[head] = list(
-                map(lambda element: element if element[0] != min_tuple[0] else (element[0], element[1] + 1),
-                    trackable_grammar[head]))
+                map(
+                    lambda element: (
+                        element
+                        if element[0] != min_tuple[0]
+                        else (element[0], element[1] + 1)
+                    ),
+                    trackable_grammar[head],
+                )
+            )
 
             if not is_non_terminal:
                 return min_tuple[0]
             else:
                 trackable_non_terminals[head] = min_tuple[1] + 1
-                non_terminals = re.findall(self.NON_TERMINAL_REGEX, min_tuple[0])
+                non_terminals = re.findall(self._NON_TERMINAL_REGEX, min_tuple[0])
                 replacements = iter(
-                    list(map(lambda element: generate_value(element), non_terminals)))
-                result = re.sub(self.NON_TERMINAL_REGEX, lambda element: next(replacements), min_tuple[0])
+                    list(map(lambda element: generate_value(element), non_terminals))
+                )
+                result = re.sub(
+                    self._NON_TERMINAL_REGEX,
+                    lambda element: next(replacements),
+                    min_tuple[0],
+                )
                 return result
 
         while not self.__is_grammar_covered(trackable_grammar, trackable_non_terminals):
