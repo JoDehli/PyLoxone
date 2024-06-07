460a003b11ca90afd00e5fa5bbcd4074f7b5705e
ThorbenCarl <thorben.carl@gmail.com>
2024-06-07 12:40:07 +0200

    update tasks, add comments, add demo test case for grammar

--- custom_components/test/fuzzing/mutation/test_mut_on_helpers.py
+++ custom_components/test/fuzzing/mutation/test_mut_on_helpers.py
@@ -1,5 +1,6 @@
 import pytest
 import logging
+import random
 
 from custom_components.loxone.helpers import (
     map_range,
@@ -18,20 +19,38 @@ from custom_components.loxone.helpers import (
 from custom_components.test.fuzzing.fuzzer_utils.MutationalFuzzer import (
     MutationalFuzzer,
 )
+from custom_components.test.fuzzing.fuzzer_utils.GrammarFuzzer import GrammarFuzzer
+from custom_components.test.fuzzing.fuzzer_utils.grammars.grammar_ipv4 import grammar_ipv4
 from custom_components.test.fuzzing.fuzzer_utils.ParamRunner import ParamRunner
 
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
-mutational_fuzzer: MutationalFuzzer = MutationalFuzzer()
-param_runner: ParamRunner = ParamRunner()
+_mutational_fuzzer: MutationalFuzzer = MutationalFuzzer()
+_grammar_fuzzer: GrammarFuzzer = GrammarFuzzer()
+_param_runner: ParamRunner = ParamRunner()
+
+
+def test_demo_get_param_set() -> None:
+    # get a list of valid grammar outputs
+    full_grammar_cov: list = _grammar_fuzzer.fuzz_grammar_coverage(
+        grammar_ipv4, "<IPv4>"
+    )
+    # choose randomly on value
+    random_valid_grammar_string = random.choice(full_grammar_cov)
+
+    # If the grammar value should be an int or float, you have to cast the string first.
+    # Get the param_set.
+    _mutational_fuzzer.fuzz([0.5, 1, "demo_string", random_valid_grammar_string], 10000)
+
+    assert True
 
 
 def test_map_range() -> None:
-    logger.info("Start of test_map_range() test.")
-    param_set: list[list] = mutational_fuzzer.fuzz([0.5, 0.5, 12.3, 123.234, 0.0], 1000)
-    param_set = param_runner.limit_param_set(param_set, 50000)
-    result: list = param_runner.run(map_range, param_set)
-    logger.info("test_map_range() test finished.")
+    _logger.info("Start of test_map_range() test.")
+    param_set: list[list] = _mutational_fuzzer.fuzz([0.5, 0.5, 12.3, 123.234, 0.0], 1000)
+    param_set = _param_runner.limit_param_set(param_set, 50000)
+    result: list = _param_runner.run(map_range, param_set)
+    _logger.info("test_map_range() test finished.")
 
     assert result[1] == 0
