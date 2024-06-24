5abba3d6b244cc6dcde80a50603f172b3dc56467
ThorbenCarl <thorben.carl@gmail.com>
2024-06-24 16:32:14 +0200

    update readme, skip demo test case

--- custom_components/test/fuzzing/grammar/test_gram_on_xxx.py
+++ custom_components/test/fuzzing/grammar/test_gram_on_xxx.py
@@ -1,11 +1,16 @@
 import logging
+import pytest
 
 from custom_components.test.fuzzing.fuzzer_utils.GrammarFuzzer import GrammarFuzzer
-from custom_components.test.fuzzing.fuzzer_utils.grammars.grammar_ipv4 import grammar_ipv4
+from custom_components.test.fuzzing.fuzzer_utils.grammars.grammar_ipv4 import (
+    grammar_ipv4,
+)
 
 logger = logging.getLogger(__name__)
 test_fuzzer = GrammarFuzzer()
 
+
+@pytest.mark.skipif(True, reason="Only dummy test case.")
 def test_dummy() -> None:
     logger.info("Start of dummy() test.")
     result = test_fuzzer.fuzz_min_cost(grammar_ipv4, "<IPv4>")
