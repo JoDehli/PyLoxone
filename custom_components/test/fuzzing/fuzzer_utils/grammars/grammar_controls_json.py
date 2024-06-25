750523981a0fb60d1c638f5f280f27f8aaa67e5a
JKortmann <johannes@kortm.net>
2024-06-25 08:20:37 +0200

    add controls key

--- custom_components/test/fuzzing/fuzzer_utils/grammars/grammar_controls_json.py
+++ custom_components/test/fuzzing/fuzzer_utils/grammars/grammar_controls_json.py
@@ -1,7 +1,7 @@
 from custom_components.test.fuzzing.fuzzer_utils.GrammarFuzzer import Grammar
 
 grammar_controls_json: Grammar = {
-    "<JSON>": ["{ <Members> }"],
+    "<JSON>": ["{ \"controls\" : { <Members> } }"],
     "<Members>": ["<ControlsPair>", "<Members>, <ControlsPair>"],
     "<ControlsPair>": ["\"<String>\": { <ControlsElement> }", "<ControlsPair>, \"<String>\": { <ControlsElement> }"],
     "<ControlsElement>": ["\"type\": \"<String>\""],
