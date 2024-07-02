8da94a1d4e6d7df3e545d38c33ca00a26de6b074
ThorbenCarl <thorben.carl@gmail.com>
2024-07-02 21:16:58 +0200

    add missing testcases

--- custom_components/test/fuzzing/mutation/test_mut_on_helpers.py
+++ custom_components/test/fuzzing/mutation/test_mut_on_helpers.py
@@ -21,9 +21,11 @@ from custom_components.test.fuzzing.fuzzer_utils.MutationalFuzzer import (
     MutationalBlackBoxFuzzer,
 )
 from custom_components.test.fuzzing.fuzzer_utils.GrammarFuzzer import GrammarFuzzer
+from custom_components.test.fuzzing.fuzzer_utils.ValuePoolFuzzer import ValuePoolFuzzer
 from custom_components.test.fuzzing.fuzzer_utils.grammar_pool import (
     grammar_ipv4,
     grammar_controls_json,
+    grammar_loxconfig_rooms_cats_json,
 )
 from custom_components.test.fuzzing.fuzzer_utils.ParamRunner import ParamRunner
 
@@ -32,6 +34,7 @@ logger = logging.getLogger(__name__)
 
 mutational_fuzzer: MutationalBlackBoxFuzzer = MutationalBlackBoxFuzzer()
 grammar_fuzzer: GrammarFuzzer = GrammarFuzzer()
+value_pool_fuzzer: ValuePoolFuzzer = ValuePoolFuzzer()
 param_runner: ParamRunner = ParamRunner()
 
 
@@ -179,22 +182,149 @@ def test_get_miniserver_type() -> None:
     assert result["failed_tests"] == 0
 
 
-@pytest.mark.skipif(True, reason="Waiting for Grammar!")
+@pytest.mark.skipif(False, reason="Not skiped!")
 @pytest.mark.timeout(300)
 def test_get_room_name_from_room_uuid() -> None:
-    assert True
+    logger.info("Start of test_get_room_name_from_room_uuid() test.")
+    param_set: list[list]
+    result: dict
+
+    # get a list of valid grammar outputs
+    full_grammar_cov: list[str]
+    full_grammar_cov = grammar_fuzzer.fuzz_grammar_coverage(
+        grammar_loxconfig_rooms_cats_json, "<JSON>"
+    )
+
+    # choose randomly on grammar string
+    random_valid_grammar_string: str
+    random_valid_grammar_string = random.choice(full_grammar_cov)
+
+    # mutate seed
+    param_set = mutational_fuzzer.fuzz([random_valid_grammar_string, "a"], 100)
+
+    # function under test needs a json object
+    for set in param_set:
+        # is param after mutation still a valid json?
+        try:
+            # Yes -> load as json
+            set[0] = json.loads(set[0])
+        except:
+            # No -> use random default value from grammar
+            logger.debug(
+                f"{set[0]} is not longer a valid json, replaced it with value from grammar."
+            )
+            set[0] = json.loads(random.choice(full_grammar_cov))
+
+    result = param_runner.run(get_room_name_from_room_uuid, param_set)
+
+    logger.info("test_get_room_name_from_room_uuid() test finished.")
+
+    assert result["failed_tests"] == 0
 
 
-@pytest.mark.skipif(True, reason="Waiting for Grammar!")
+@pytest.mark.skipif(False, reason="Not skiped!")
 @pytest.mark.timeout(300)
 def test_get_cat_name_from_cat_uuid() -> None:
-    assert True
+    logger.info("Start of test_get_cat_name_from_cat_uuid() test.")
+    param_set: list[list]
+    result: dict
+
+    # get a list of valid grammar outputs
+    full_grammar_cov: list[str]
+    full_grammar_cov = grammar_fuzzer.fuzz_grammar_coverage(
+        grammar_loxconfig_rooms_cats_json, "<JSON>"
+    )
+
+    # choose randomly on grammar string
+    random_valid_grammar_string: str
+    random_valid_grammar_string = random.choice(full_grammar_cov)
+
+    # mutate seed
+    param_set = mutational_fuzzer.fuzz([random_valid_grammar_string, "a"], 100)
+
+    # function under test needs a json object
+    for set in param_set:
+        # is param after mutation still a valid json?
+        try:
+            # Yes -> load as json
+            set[0] = json.loads(set[0])
+        except:
+            # No -> use random default value from grammar
+            logger.debug(
+                f"{set[0]} is not longer a valid json, replaced it with value from grammar."
+            )
+            set[0] = json.loads(random.choice(full_grammar_cov))
+
+    result = param_runner.run(get_cat_name_from_cat_uuid, param_set)
+
+    logger.info("test_get_cat_name_from_cat_uuid() test finished.")
+
+    assert result["failed_tests"] == 0
 
 
-@pytest.mark.skipif(True, reason="Waiting for Grammar!")
+@pytest.mark.skipif(False, reason="Not skiped!")
 @pytest.mark.timeout(300)
 def test_add_room_and_cat_to_value_values() -> None:
-    assert True
+    logger.info("Start of test_add_room_and_cat_to_value_values() test.")
+    param_set: list[list]
+    result: dict
+
+    # get a list of valid grammar outputs
+    full_grammar_cov: list[str]
+    full_grammar_cov = grammar_fuzzer.fuzz_grammar_coverage(
+        grammar_loxconfig_rooms_cats_json, "<JSON>"
+    )
+
+    # choose randomly on grammar string
+    random_valid_grammar_string: str
+    random_valid_grammar_string = random.choice(full_grammar_cov)
+
+    random_valid_grammar_string_2: list[list] = value_pool_fuzzer.fuzz(["DICT"], 1)
+    random_valid_grammar_string_2: list = random.choice(random_valid_grammar_string_2)
+    random_valid_grammar_string_2: dict = random_valid_grammar_string_2[0]
+    random_valid_grammar_string_2: str = str(random_valid_grammar_string_2)
+
+    # mutate seed
+    param_set = mutational_fuzzer.fuzz(
+        [random_valid_grammar_string, random_valid_grammar_string_2], 100
+    )
+
+    # function under test needs a json object
+    for set in param_set:
+        # is param after mutation still a valid json?
+        try:
+            # Yes -> load as json
+            set[0] = json.loads(set[0])
+        except:
+            # No -> use random default value from grammar
+            logger.debug(
+                f"{set[0]} is not longer a valid json, replaced it with value from grammar."
+            )
+            set[0] = json.loads(random.choice(full_grammar_cov))
+
+        try:
+            # Yes -> load as json
+            logger.debug(f"{set[1]} is type of {type(set[1]).__name__}.")
+            if set[1].isdigit():
+                logger.debug(f"{set[1]} is not a valid JSON and type of {type(set[1]).__name__}.")
+                set[1] = json.loads('{"set[1]":123')
+            else:
+                set[1] = json.loads(set[1])
+                logger.debug(f"{set[1]} is a valid JSON and type of {type(set[1]).__name__}.")
+        except:
+            # No -> use default value from value pool
+            logger.debug(
+                f"{set[1]} is not longer a valid json, replaced it with {random_valid_grammar_string_2}."
+            )
+            set[1] = json.loads(random_valid_grammar_string_2)
+
+        logger.debug(f"{set[0]} and {set[1]} is a param_set.")
+
+    result = param_runner.run(add_room_and_cat_to_value_values, param_set)
+
+    logger.info("test_add_room_and_cat_to_value_values() test finished.")
+
+    assert result["failed_tests"] == 0
 
 
 @pytest.mark.skipif(False, reason="Not skiped!")
