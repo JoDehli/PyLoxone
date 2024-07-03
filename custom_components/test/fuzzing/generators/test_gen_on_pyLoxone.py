6e74859db17e37156a2bd1e5cfbf0ed9318d1295
David Sievers <dsievers@mail.hs-ulm.de>
2024-07-03 08:56:31 +0000

    Updated: code with types, UML Found test on PyLoxone for generators which runs (but is trivial)

--- custom_components/test/fuzzing/generators/test_gen_on_pyLoxone.py
+++ custom_components/test/fuzzing/generators/test_gen_on_pyLoxone.py
@@ -4,31 +4,31 @@ import logging
 from custom_components.test.fuzzing.fuzzer_utils.ValuePoolFuzzer import ValuePoolFuzzer
 from custom_components.test.fuzzing.fuzzer_utils.GeneratorFuzzer import GeneratorFuzzer
 from custom_components.test.fuzzing.fuzzer_utils.GeneratorRunner import GeneratorRunner
-from custom_components.test.fuzzing.generators.DummyClass import DummyClass
 from custom_components.loxone.climate import LoxoneRoomControllerV2
+from custom_components.loxone.api import LxToken
 
 # Logger setup
 logger = logging.getLogger(__name__)
 
 # Fuzzing and Runner setup
 value_pool_fuzzer = ValuePoolFuzzer()
-generator_fuzzer = GeneratorFuzzer(value_pool_fuzzer, 3)
+generator_fuzzer = GeneratorFuzzer(value_pool_fuzzer, 2)
 generator_runner = GeneratorRunner()
 
 #@pytest.mark.timeout(300)
-#def test_DummyClass() -> None:
-#    logger.info("Start of DummyClass test.")
+#def test_climate_LoxoneRoomControllerV2() -> None:
+#    logger.info("Start test of LoxoneRoomControllerV2 from climate.py.")
 #    
 #    # Define the start methods and parameters for fuzzing
 #    start_methods = ['__init__']
-#    max_sequence_length = 4
-#    num_sequences = 10
+#    max_sequence_length = 8
+#    num_sequences = 50
 #    
 #    # Generate fuzzed sequences
-#    generated_sequences = generator_fuzzer.fuzz(DummyClass, start_methods, max_sequence_length, num_sequences)
+#    generated_sequences = generator_fuzzer.fuzz(LoxoneRoomControllerV2, start_methods, max_sequence_length, num_sequences)
 #    
-#    # Run the sequences on DummyClass
-#    results = generator_runner.run(DummyClass, generated_sequences)
+#    # Run the sequences on LoxoneRoomControllerV2
+#    results = generator_runner.run(LoxoneRoomControllerV2, generated_sequences)
 #    
 #    # Extract passed and failed test counts
 #    passed_tests, failed_tests = results
@@ -36,11 +36,11 @@ generator_runner = GeneratorRunner()
 #    # Log the results
 #    logger.info(f"Passed tests: {passed_tests}, Failed tests: {failed_tests}")
 #    
-#    logger.info("DummyClass test finished.")
+#    logger.info("LoxoneRoomControllerV2 test finished.")
 
 @pytest.mark.timeout(300)
-def test_climate_LoxoneRoomControllerV2() -> None:
-    logger.info("Start test of LoxoneRoomControllerV2 from climate.py.")
+def test_api_LxToken() -> None:
+    logger.info("Start test of LxToken from api.py.")
     
     # Define the start methods and parameters for fuzzing
     start_methods = ['__init__']
@@ -48,10 +48,10 @@ def test_climate_LoxoneRoomControllerV2() -> None:
     num_sequences = 50
     
     # Generate fuzzed sequences
-    generated_sequences = generator_fuzzer.fuzz(LoxoneRoomControllerV2, start_methods, max_sequence_length, num_sequences)
+    generated_sequences = generator_fuzzer.fuzz(LxToken, start_methods, max_sequence_length, num_sequences)
     
     # Run the sequences on LoxoneRoomControllerV2
-    results = generator_runner.run(LoxoneRoomControllerV2, generated_sequences)
+    results = generator_runner.run(LxToken, generated_sequences)
     
     # Extract passed and failed test counts
     passed_tests, failed_tests = results
@@ -59,7 +59,8 @@ def test_climate_LoxoneRoomControllerV2() -> None:
     # Log the results
     logger.info(f"Passed tests: {passed_tests}, Failed tests: {failed_tests}")
     
-    logger.info("LoxoneRoomControllerV2 test finished.")
+    logger.info("LxToken test finished.")
 
+#for debugging purposes
 if __name__ == "__main__":
     pytest.main()
