db1b37b85de4b51b1a3e0d0ab48d08b42defeb89
David Sievers <dsievers@mail.hs-ulm.de>
2024-07-04 10:32:50 +0000

    removed code for debugging according to change request

--- custom_components/test/fuzzing/generators/test_gen_on_pyLoxone.py
+++ custom_components/test/fuzzing/generators/test_gen_on_pyLoxone.py
@@ -15,29 +15,6 @@ value_pool_fuzzer = ValuePoolFuzzer()
 generator_fuzzer = GeneratorFuzzer(value_pool_fuzzer, 2)
 generator_runner = GeneratorRunner()
 
-#@pytest.mark.timeout(300)
-#def test_climate_LoxoneRoomControllerV2() -> None:
-#    logger.info("Start test of LoxoneRoomControllerV2 from climate.py.")
-#    
-#    # Define the start methods and parameters for fuzzing
-#    start_methods = ['__init__']
-#    max_sequence_length = 8
-#    num_sequences = 50
-#    
-#    # Generate fuzzed sequences
-#    generated_sequences = generator_fuzzer.fuzz(LoxoneRoomControllerV2, start_methods, max_sequence_length, num_sequences)
-#    
-#    # Run the sequences on LoxoneRoomControllerV2
-#    results = generator_runner.run(LoxoneRoomControllerV2, generated_sequences)
-#    
-#    # Extract passed and failed test counts
-#    passed_tests, failed_tests = results
-#    
-#    # Log the results
-#    logger.info(f"Passed tests: {passed_tests}, Failed tests: {failed_tests}")
-#    
-#    logger.info("LoxoneRoomControllerV2 test finished.")
-
 @pytest.mark.timeout(300)
 def test_api_LxToken() -> None:
     logger.info("Start test of LxToken from api.py.")
@@ -60,7 +37,3 @@ def test_api_LxToken() -> None:
     logger.info(f"Passed tests: {passed_tests}, Failed tests: {failed_tests}")
     
     logger.info("LxToken test finished.")
-
-#for debugging purposes
-if __name__ == "__main__":
-    pytest.main()
