a010dc60ec11df60e294225b77f08ce50f57c93b
JoDehli <joachim.dehli@gmail.com>
2026-07-19 11:15:41 +0200

    fix: Bug Report: System Health crashes when Miniserver serial is unavailable #500

--- custom_components/loxone/system_health.py
+++ custom_components/loxone/system_health.py
@@ -21,9 +21,22 @@ def async_register(
 async def system_health_info(hass: HomeAssistant) -> dict[str, Any]:
     """Get info for the info page."""
     for k, v in hass.data[DOMAIN].items():
+        if hasattr(v, "miniserver"):
+            miniserver_serial = v.miniserver.serial
+            software_version = v.miniserver.software_version
+            project_name = v.miniserver.lox_config.json["msInfo"]["projectName"]
+            local_url = v.miniserver.lox_config.json["msInfo"]["localUrl"]
+            remote_url = v.miniserver.lox_config.json["msInfo"]["remoteUrl"]
+        else:
+            miniserver_serial = "Unavailable"
+            software_version = "Unavailable"
+            project_name = "Unavailable"
+            local_url = "Unavailable"
+            remote_url = "Unavailable"
         return {
-            "Loxone Miniserver Serial": v.serial,
-            "Project Name": v.project_name,
-            "Local Url": v.local_url,
-            "Loxone Software Version": v.software_version,
+            "Loxone Miniserver Serial": miniserver_serial,
+            "Project Name": project_name,
+            "Local Url": local_url,
+            "Remote Url": remote_url,
+            "Loxone Software Version": software_version,
         }
